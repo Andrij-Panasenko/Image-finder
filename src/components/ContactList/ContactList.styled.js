ae00670d5ffef9d689f7f37ecc9973c72ec3d184
a.panasenko <andrii.panasenko295@gmail.com>
2023-11-09 23:03:15 +0200

    clear

--- src/components/ContactList/ContactList.styled.js
+++
@@ -1,5 +0,0 @@
-import styled from 'styled-components';
-
-export const Notify = styled.p`
-  text-align: center;
-`;
--- src/components/Title/Title.js
+++
@@ -1,5 +0,0 @@
-import { ContactsTitle } from './Title.styled';
-
-export const Title = ({ title }) => {
-   return <ContactsTitle>{title}</ContactsTitle>;
-};
