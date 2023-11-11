550460cb234cae0b400d529bfa35ebf90008ffe5
a.panasenko <andrii.panasenko295@gmail.com>
2023-11-11 15:15:34 +0200

    logic search

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import { GlobalStyle } from './GlobalStyle';
 import { Component } from 'react';
+
+import toast, { Toaster } from 'react-hot-toast';
 import { SearchBar } from './SearchBar/SerachBar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Button } from './Button/Button';
 import { fetchItem } from './api/api';
-import { Loader } from './Loader/Loader';
+// import { Loader } from './Loader/Loader';
 
 export class App extends Component {
   state = {
@@ -17,24 +19,41 @@ export class App extends Component {
   async componentDidUpdate(prevProps, prevState) {
     const { query, page } = this.state;
 
+    const searchQuery = query.split('/')[1].trim();
+
+    if (searchQuery === '') {
+      toast.error('Search field should be filled');
+      this.setState({ query: '' });
+      return;
+    }
+
     try {
-      
       if (prevState.query !== query || prevState.page !== page) {
-        this.setState({ isLoading: true });
-        const requestedImages = await fetchItem(query, page);
+        // this.setState({ isLoading: true });
+
+        const requestedImages = await fetchItem(searchQuery, page);
+
+        if (requestedImages.hits.length === 0) {
+          toast.error('There are no results for your request');
+          this.setState({query: ''});
+          return;
+        }
+
         this.setState({ images: requestedImages.hits });
       }
     } catch (error) {
+      toast.error('Ooops! Something went wrong. Try reloading page!');
     } finally {
-      this.setState({ isLoading: false });
+      // this.setState({ isLoading: false });
     }
-  };
+  }
 
   searchQueryHandler = evt => {
     evt.preventDefault();
     const searchQuery = evt.target.query.value;
+    const query = searchQuery.toLowerCase();
     this.setState({
-      query: searchQuery,
+      query: `${Date.now()}/${query}`,
       page: 1,
       images: [],
     });
@@ -50,18 +69,20 @@ export class App extends Component {
 
   render() {
     const { images, isLoading } = this.state;
+
     return (
       <>
         <GlobalStyle />
+        <Toaster />
         <SearchBar onSubmit={this.searchQueryHandler} />
-        {isLoading && <Loader />}
+        {/* {isLoading && <Loader />} */}
         {images.length > 0 && (
-          <ImageGallery images={images} />
+          <>
+            <ImageGallery images={images} />
+            <Button onLoad={this.handleLoadMore} />
+          </>
         )}
-        <Button onLoad={this.handleLoadMore} />
       </>
     );
   }
 }
-
-
