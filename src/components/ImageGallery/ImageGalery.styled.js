54465a56d4f7a415c7924264e596f4393607622b
a.panasenko <andrii.panasenko295@gmail.com>
2023-11-11 23:47:12 +0200

    Update ImageGalery.styled.js

--- src/components/ImageGallery/ImageGalery.styled.js
+++ src/components/ImageGallery/ImageGalery.styled.js
@@ -4,12 +4,12 @@ export const List = styled.ul`
   display: grid;
   max-width: calc(100vw - 48px);
   grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
-  grid-gap: 16px;
+  grid-gap: ${p => p.theme.spacing(8)};
   margin-top: 0;
   margin-bottom: 0;
   padding: 0;
-  padding-top: 20px;
-  padding-bottom: 20px;
+  padding-top: ${p => p.theme.spacing(10)};
+  padding-bottom: ${p => p.theme.spacing(10)};
 
   margin: 0 auto;
 `;
