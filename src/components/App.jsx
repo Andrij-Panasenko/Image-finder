5c1e2586107ea7a1403e9557329a1bf42a776ec2
a.panasenko <andrii.panasenko295@gmail.com>
2023-11-11 18:14:05 +0200

    ок

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,13 +6,13 @@ import { SearchBar } from './SearchBar/SerachBar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Button } from './Button/Button';
 import { fetchItem } from './api/api';
-import { Loader } from './Loader/Loader';
+// import { Loader } from './Loader/Loader';
 
 export class App extends Component {
   state = {
     images: [],
     query: '',
-    isLoading: false,
+    // isLoading: false,
     page: 1,
   };
 
@@ -23,33 +23,47 @@ export class App extends Component {
 
     if (searchQuery === '') {
       toast.error('Search field should be filled');
-      this.setState({ query: '' });
       return;
     }
 
     try {
       if (prevState.query !== query || prevState.page !== page) {
-        this.setState({ isLoading: true });
+        // this.setState({ isLoading: true });
 
         const requestedImages = await fetchItem(searchQuery, page);
 
-        if (requestedImages.hits.length === 0) {
+
+        const totalCards = requestedImages.totalHits;
+        const totalPages = Math.ceil(totalCards / 12);
+        // console.log('totalPages', totalPages)
+        // console.log('page', page)
+        // console.log('totalCards', totalCards)
+
+
+        console.log(requestedImages);
+        
+
+
+        if (requestedImages.hits.length === 0 || page === totalPages +1) {
           toast.error('There are no results for your request');
-          this.setState({ query: '' });
           return;
         }
 
-        this.setState({ images: requestedImages.hits });
+        this.setState(prevState => ({
+          images: prevState.images.length === 0 ?
+            requestedImages.hits : [...prevState.images, ...requestedImages.hits]
+        }));
       }
     } catch (error) {
       toast.error('Ooops! Something went wrong. Try reloading page!');
     } finally {
-      this.setState({ isLoading: false });
+      // this.setState({ isLoading: false });
     }
   }
 
   searchQueryHandler = evt => {
     evt.preventDefault();
+
     const searchQuery = evt.target.query.value;
     const query = searchQuery.toLowerCase();
     this.setState({
@@ -75,7 +89,7 @@ export class App extends Component {
         <GlobalStyle />
         <Toaster />
         <SearchBar onSubmit={this.searchQueryHandler} />
-        {isLoading && <Loader />}
+        {/* {isLoading && <Loader />} */}
         {images.length > 0 && (
           <>
             <ImageGallery images={images} />
--- src/components/api/api.js
+++ src/components/api/api.js
@@ -5,7 +5,7 @@ const API_KEY = '40600754-9a7de304f3ec5f2ad2323bb7e';
 
 export const fetchItem = async (query, page) => {
    const response = await axios.get(
-     `${BASE_URL}?key=${API_KEY}&q=${query}&page=${page}&image_type=photo&orientation=horizontal&per_page=12`
+     `${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&per_page=12&page=${page}`
    );
    return response.data;
 }
