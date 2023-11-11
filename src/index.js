fe1c24091e18e984eeb5031376d4c316d615e993
a.panasenko <andrii.panasenko295@gmail.com>
2023-11-11 23:58:58 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,12 +9,9 @@ const theme = {
     red: '#E24C4B',
     lightGrey: 'rgba(46, 47, 66, 0.4)',
     orange: '#FFD466',
-    imageBoxShadow:
-      '0px 1px 3px 0px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px rgba(0, 0, 0, 0.12)',
-    boxShadow:
-      '0px 2px 4px -1px rgba(0, 0, 0, 0.2), 0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12)',
-    loadMoreBoxShadow:
-      '0px 3px 1px -2px rgba(0, 0, 0, 0.2), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12)',
+    imageBoxShadow: '0px 1px 3px 0px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px rgba(0, 0, 0, 0.12)',
+    boxShadow: '0px 2px 4px -1px rgba(0, 0, 0, 0.2), 0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12)',
+    loadMoreBoxShadow:  '0px 3px 1px -2px rgba(0, 0, 0, 0.2), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12)',
     background: '#f5f5f5',
     headerBackground: '#6d3aa1',
     loadMoreBcg: '#3f51b5',
